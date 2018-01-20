622ca318f1a63e964dfa3ffb52807dd888744ceb
BDabrowski <bdabrowski@live.com>
2018-01-19 23:32:56 -0600

    Added two files

---
+++ Neon Leon/Tutorial.swift
@@ -0,0 +1,25 @@
+//
+//  Tutorial.swift
+//  
+//
+//  Created by BDabrowski on 1/16/18.
+//
+
+import Foundation
+import SpriteKit
+
+class Tutorial: SKScene {
+    
+    override func didMove(to view: SKView) {
+        let dimmer = SKSpriteNode(imageNamed: "Dimmer")
+        dimmer.position = CGPoint(x: 768, y: 1218)
+        addChild(dimmer)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let newScene = GameScene(fileNamed: "MainMenu")
+        newScene!.scaleMode = .aspectFill
+        let fade = SKTransition.fade(withDuration: 0.5)//SKTransition.flipHorizontal(withDuration: 0.5)
+        self.view?.presentScene(newScene!, transition: fade)
+    }
+}
